0fafc4813969dfc79c7720d0ba09c6ed21fe9054
martenmatrix <derapfelesser@aol.de>
2022-04-08 00:39:48 +0200

    Add routes for pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Homepage from './components/Homepage';
+import AboutMe from './components/Homepage';
+import Projects from './components/Homepage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={Homepage}/>
+                    <Route path="about" element={AboutMe}/>
+                    <Route path="projects" element={Projects}/>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
