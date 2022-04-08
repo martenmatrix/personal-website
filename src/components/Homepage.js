b47be7ad540bf2aed1389b2193d83f6a38bff2d0
martenmatrix <derapfelesser@aol.de>
2022-04-08 20:09:22 +0200

    Add links to GitHub and StackOverflow

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import './SpotifyCurrentlyPlaying';
 import '../styles/Homepage.css';
 
 function Links() {
@@ -7,6 +8,8 @@ function Links() {
             <ul className="links">
                 <li><Link to="about">about me</Link></li>
                 <li><Link to="projects">projects</Link></li>
+                <li><a href="https://github.com/martenmatrix" target="_blank" rel="noreferrer">github</a></li>
+                <li><a href="https://stackoverflow.com/users/14231863/marten" target="_blank" rel="noreferrer">stack overflow</a></li>
             </ul>
         </nav>
     )
