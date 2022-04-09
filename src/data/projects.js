17a6833237484745dba92ab0e71665cbc5599c50
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-10 00:17:51 +0200

    Add projects data file

---
+++ src/data/projects.js
@@ -0,0 +1,9 @@
+const projects = [
+    {
+        id: 'fifo-calc',
+        github: 'https://github.com/martenmatrix/capital-gains-trading-calculator',
+        readme: 'https://raw.githubusercontent.com/martenmatrix/capital-gains-trading-calculator/master/README.md'
+    }
+]
+
+export default projects;
