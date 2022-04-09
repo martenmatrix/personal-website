54c1322d128be7e5ddc3566f66d838917d64aac5
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-10 00:18:24 +0200

    Add functions to fetch README content from GitHub

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,43 @@
+import { useState, useEffect } from 'react';
 import { Route, Routes, useParams } from 'react-router-dom';
+import projects from '../data/projects';
 
 function ProjectOverview() {
     const params = useParams();
+    const [project, setProject] = useState();
+    const [readMe, setReadMe] = useState();
+
+    useEffect(() => {
+        const getProject = (id) => {
+            const index = projects.findIndex((project) => project.id === id);
+            const project = projects[index];
+            setProject(project);
+        }
+
+        getProject(params.projectId);
+
+    }, [params.projectId]);
+
+    useEffect(() => {
+        const getReadMe = async () => {
+            if (!project) {
+                setReadMe();
+                return;
+            }
+            const response = await fetch(project.readme, {
+                method: 'GET',
+            });
+            const readme = await response.text();
+            setReadMe(readme);
+        }
+
+        getReadMe();
+        
+    }, [project]);
 
     return (
         <div className="project-overview">
-            {params.projectId}
+            <pre>{JSON.stringify(readMe)}</pre>
         </div>
     )
 }
