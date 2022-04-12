6c4f514a7d0702c96fe679f4fac986caa0f4750f
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-12 23:15:47 +0200

    Replace about me section with contact option

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,7 +7,7 @@ function Links() {
     return (
         <nav>
             <ul className="links">
-                <li><Link to="about">about me</Link></li>
+                <li><a href="mailto:privacy.shadier_0b@icloud.com?body=⚠️ I hide my real mail address to protect me from spammers. Your mail will get forwarded to my real account and I will answer with my mail address unhidden." rel="noreferrer">contact</a></li>
                 <li><Link to="projects">projects</Link></li>
                 <li><a href="https://github.com/martenmatrix" target="_blank" rel="noreferrer">github<img src={arrowUpRight} alt="arrow up right" /></a></li>
                 <li><a href="https://stackoverflow.com/users/14231863/marten" target="_blank" rel="noreferrer">stack overflow<img src={arrowUpRight} alt="arrow up right" /></a></li>
