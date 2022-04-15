0160cc3e74472bba523b5f48c0e92ad96e761672
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-16 01:23:23 +0200

    Add function to recognize if link can be passed to react router link component

--- src/components/LinkDesign.js
+++ src/components/LinkDesign.js
@@ -2,8 +2,15 @@ import { Link } from 'react-router-dom';
 import '../styles/Link.css';
 
 function LinkDesign({ a, children, className, ...rest }) {
+
     const isAbsoluteLink = (link) => {
-        const matches = link.match(/https?+:\/\//gi);
+        const matches = link.match(/^(https?)/gi);
+        const hasMatch = !!matches;
+        return hasMatch;
+    }
+
+    const unableToParseByReactRouter = (link) => {
+        const matches = link.match(/^(https?|mailto|#)/gi);
         const hasMatch = !!matches;
         return hasMatch;
     }
