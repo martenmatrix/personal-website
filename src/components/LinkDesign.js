23652f5f044b2956925ad411aa9c99a9f63d774b
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-16 01:09:48 +0200

    Refactor links visibile on Homepage to own component

---
+++ src/components/LinkDesign.js
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import '../styles/Link.css';
+
+function LinkDesign({ a, children, className, ...rest }) {
+    const isAbsoluteLink = (link) => {
+        const matches = link.match(/https?+:\/\//gi);
+        const hasMatch = !!matches;
+        return hasMatch;
+    }
+
+    const newClassName = `link-design ${className}`;
+
+    if (isAbsoluteLink) {
+        return <a href={a} className={newClassName} {...rest}>{children}</a>;
+    } else {
+        return <Link to={a} className={newClassName} {...rest}>{children}</Link>;
+    }
+}
+
+export default LinkDesign;
