0dc9da3489d963b2f1fd4f94e6671dfa13753475
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-16 01:52:58 +0200

    Replace anchor tags and react router links with LinkComponent

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,14 @@
-import { Link } from 'react-router-dom';
-import arrowUpRight from '../img/arrow-up-right.svg';
-import './SpotifyCurrentlyPlaying';
+import LinkDesign from './LinkDesign';
 import '../styles/Homepage.css';
 
 function Links() {
     return (
         <nav>
             <ul className="links">
-                <li><a href="mailto:privacy.shadier_0b@icloud.com?body=⚠️ I hide my real mail address to protect me from spammers. Your mail will get forwarded to my real account and I will answer with my mail address unhidden." rel="noreferrer">contact</a></li>
-                <li><Link to="projects">projects</Link></li>
-                <li><a href="https://github.com/martenmatrix" target="_blank" rel="noreferrer">github<img src={arrowUpRight} alt="arrow up right" /></a></li>
-                <li><a href="https://stackoverflow.com/users/14231863/marten" target="_blank" rel="noreferrer">stack overflow<img src={arrowUpRight} alt="arrow up right" /></a></li>
+                <li><LinkDesign to="mailto:privacy.shadier_0b@icloud.com?body=⚠️ I hide my real mail address to protect me from spammers. Your mail will get forwarded to my real account and I will answer with my mail address unhidden.">contact</LinkDesign></li>
+                <li><LinkDesign to="projects">projects</LinkDesign></li>
+                <li><LinkDesign to="https://github.com/martenmatrix" target="_blank">github</LinkDesign></li>
+                <li><LinkDesign to="https://stackoverflow.com/users/14231863/marten" target="_blank">stack overflow</LinkDesign></li>
             </ul>
         </nav>
     )
