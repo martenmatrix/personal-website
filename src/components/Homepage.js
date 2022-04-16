4f1dfece159dad7846c32d82bf85d1b380e809f7
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-16 22:16:35 +0200

    Make links being generated on the homepage from the links data file

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,19 @@
 import LinkDesign from './LinkDesign';
+import links from '../data/links'
 import '../styles/Homepage.css';
 
 function Links() {
+
+    const getTargetValue = (linkObject) => linkObject.newTab ? '_blank' : '_self';
+
     return (
         <nav>
             <ul className="links">
-                <li><LinkDesign to="mailto:privacy.shadier_0b@icloud.com?body=⚠️ I hide my real mail address to protect me from spammers. Your mail will get forwarded to my real account and I will answer with my mail address unhidden.">contact</LinkDesign></li>
-                <li><LinkDesign to="projects">projects</LinkDesign></li>
-                <li><LinkDesign to="https://github.com/martenmatrix" target="_blank">github</LinkDesign></li>
-                <li><LinkDesign to="https://stackoverflow.com/users/14231863/marten" target="_blank">stack overflow</LinkDesign></li>
+                {links.map((linkObject, index) => (
+                    <li key={index}>
+                        <LinkDesign to={linkObject.href} target={getTargetValue(linkObject)}>{linkObject.title}</LinkDesign>
+                    </li>
+                ))}
             </ul>
         </nav>
     )
