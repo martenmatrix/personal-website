f1af665c09bc68eccd96eb2ac16fc6e2b502bb2d
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-16 21:47:49 +0200

    Add links from homepage to links data file

--- src/data/links.js
+++ src/data/links.js
@@ -1,5 +1,8 @@
 const links = [
-
+    { title: 'contact', href: 'mailto:privacy.shadier_0b@icloud.com?body=⚠️ I hide my real mail address to protect me from spammers. Your mail will get forwarded to my real account and I will answer with my mail address unhidden.' },
+    { title: 'projects', href: 'projects' },
+    { title: 'github', href: 'https://github.com/martenmatrix'},
+    { title: 'stack overflow', href: 'https://stackoverflow.com/users/14231863/marten'}
 ];
 
 export default links;
