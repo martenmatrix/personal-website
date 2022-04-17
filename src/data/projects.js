4965c0c405bd6b2fa3185b19a2819624fc30ca26
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-17 15:41:49 +0200

    Add title and description to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,6 +13,8 @@ import matrixaccsFrontend from '../img/matrixaccs-frontend.gif';
 
 const projects = [
     {
+        title: 'Capital Gains Calculator',
+        description: "Calculate your capital gains with the FiFo method with CSV's from Trading212 or Revolut.",
         id: 'fifo-calc',
         titleImage: fifoCalc,
         deploy: 'https://trading-csv-fifo-calculator.web.app/',
@@ -20,6 +22,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/capital-gains-trading-calculator/master/README.md',
     },
     {
+        title: 'Calculator',
+        description: 'Simple calculator, which calculates results without the dangerous eval() function.',
         id: 'calculator',
         titleImage: calculator,
         deploy: 'https://martenmatrix.github.io/calculator/',
@@ -27,6 +31,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/calculator/main/README.md',
     },
     {
+        title: 'Weather App',
+        description: 'Uses the OpenWeatherMap API to display the current weather for an entered location.',
         id: 'weather-app',
         titleImage: weatherApp,
         deploy: 'https://martenmatrix.github.io/weather-app/',
@@ -34,6 +40,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/weather-app/main/README.md',
     },
     {
+        title: 'Weather App',
+        description: "Play TicTacToe against a friend or an AI, which uses a backtracking algorithm. Try to beat the AI, you won't.",
         id: 'tic-tac-toe',
         titleImage: ticTacToe,
         deploy: 'https://martenmatrix.github.io/tic-tac-toe/',
@@ -41,6 +49,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/tic-tac-toe/main/README.md',
     },
     {
+        title: 'Shopping Cart',
+        description: 'Visit a fake online-store, where the exact state of the page is saved in the URL.',
         id: 'shopping-cart',
         titleImage: shoppingCart,
         deploy: 'https://martenmatrix.github.io/shopping-cart/',
@@ -48,6 +58,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/shopping-cart/main/README.md',
     },
     {
+        title: 'CV Generator',
+        description: 'Generate your own Curriculum Vitae.',
         id: 'cv-generator',
         titleImage: CVGenerator,
         deploy: 'https://martenmatrix.github.io/cv-generator/',
@@ -55,6 +67,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/cv-generator/main/README.md',
     },
     {
+        title: 'Cocktail Mixer',
+        description: 'This is the frontend and backend code for a physical Cocktail Mixer machine. A vague build guide is also included.',
         id: 'cocktail-mixer',
         titleImage: cocktailMixer,
         deploy: '#',
@@ -62,6 +76,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/cocktail-mixer/main/README.md',
     },
     {
+        title: 'Battleship',
+        description: 'Play the classic Battleship Game against against a computer, which uses a bit of AI.',
         id: 'battleship',
         titleImage: battleship,
         deploy: 'https://martenmatrix.github.io/odin-battleship/',
@@ -69,6 +85,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/battleship/main/README.md'
     },
     {
+        title: 'Memory Card Game',
+        description: "This application puts your memory to the test. You'll have to click each image only one time.",
         id: 'memory-card',
         titleImage: MemoryCard,
         deploy: 'https://martenmatrix.github.io/memory-card/',
@@ -88,6 +106,8 @@ const projects = [
         hide: true
     },
     {
+        title: 'ToDo List',
+        description: "Create ToDo's, assign them with a priority, date and description or group them.",
         id: 'todo-list',
         titleImage: todoList,
         deploy: 'https://martenmatrix.github.io/todo-list/',
@@ -95,6 +115,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/todo-list/main/README.md',
     },
     {
+        title: 'Mobile Menu',
+        description: 'Lightweight and customizable mobile menu for the web.',
         id: 'mobile-menu',
         titleImage: MobileMenu,
         deploy: 'https://martenmatrix.github.io/mobile-menu/',
@@ -102,6 +124,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/mobile-menu/main/README.md',
     },
     {
+        title: 'Landing Page for matrixaccs',
+        description: 'Landing Page for a business, which sells utilities to help customers secure limited products.',
         id: 'matrixaccs-frontend',
         titleImage: matrixaccsFrontend,
         deploy: 'https://www.matrixaccs.com',
@@ -109,6 +133,8 @@ const projects = [
         readme: 'https://raw.githubusercontent.com/martenmatrix/matrixaccs-frontend-public/master/README.md',
     },
     {
+        title: 'Personal Website',
+        description: 'Template for a slick and lightweight Personal Website, which can easily be customized.',
         id: 'personal-website',
         deploy: 'https://marten.codes',
         git: 'https://github.com/martenmatrix/personal-website',
