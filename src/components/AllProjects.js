535fb8112a50548fbf6e49c4ba2328e0ce1ea4b3
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-17 13:28:47 +0200

    Filter out unhidden projects and sort by order

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -1,7 +1,40 @@
+import projects from '../data/projects';
 import '../styles/AllProjects.css';
 
+function ProjectContainer({ project }) {
+    	return null;
+}
+
 function AllProjects() {
-    return null;
+    const unhiddenProjects = projects.filter((project) => !project.hide);
+    const sortedProjects = unhiddenProjects.sort((projectA, projectB) => {
+        const posA = projectA.order;
+        const posB = projectB.order;
+
+        if (posA && posB) {
+            if (posA > posB) {
+                return -1;
+            } else if (posB > posA) {
+                return 1;
+            }
+        }
+
+        if (posA && !posB) {
+            return -1;
+        }
+
+        if (!posA && posB) {
+            return 1;
+        }
+
+        return 0;
+    });
+
+    return (
+        <div className="all-projects">
+            {sortedProjects.map((project) => <ProjectContainer key={project.id} project={project} />)}
+        </div>
+    )
 }
 
 export default AllProjects;
