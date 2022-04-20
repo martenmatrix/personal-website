84fb4e792c42b87d65c114d91425066e90420e63
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-21 00:00:52 +0200

    Refactor to use new React 18 root API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import ErrorBoundary from './components/ErrorBoundary';
 import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
     <React.StrictMode>
         <ErrorBoundary>
             <App />
         </ErrorBoundary>
     </React.StrictMode>,
-    document.getElementById('root')
 );
