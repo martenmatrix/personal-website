fefd71df18ef819614b6cbf9d41bc37dd3a15051
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-04-21 13:31:09 +0200

    Set default value for loading if not specified

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import '../styles/Image.css';
 
-function Image({ src, alt, className, loading = 'eager', ...rest}) {
+function Image({ src, alt, className, loading = 'auto', ...rest}) {
     const [hasLoaded, setHasLoaded] = useState(false);
     const classes = `${hasLoaded ? '' : 'loading '}loading-image${className ? ' ' + className : ''}`;
 
