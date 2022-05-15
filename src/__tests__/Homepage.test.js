0ebd0687c757f0c87c783b4c4dcd8fb9780339b5
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-15 14:54:58 +0200

    Move render function in each test

--- src/__tests__/Homepage.test.js
+++ src/__tests__/Homepage.test.js
@@ -14,25 +14,32 @@ jest.mock('../components/LinkDesign.js', () => {
     return ({ to, target, children }) => <a href={to} target={target}>{children}</a>;
 });
 
-render(<Homepage />);
-const navigationLinks = screen.getAllByRole('listitem');
-const navigationLinksAnchor = screen.getAllByRole('link');
-
 test('animation delay increases 200ms for every link', () => {
+    render(<Homepage />);
+    const navigationLinks = screen.getAllByRole('listitem');
+
     expect(navigationLinks[0]).toHaveStyle('animation-delay: 0ms');
     expect(navigationLinks[1]).toHaveStyle('animation-delay: 200ms');
     expect(navigationLinks[2]).toHaveStyle('animation-delay: 400ms');
 });
 
 test('title is displayed as child of anchor tag', () => {
+    render(<Homepage />);
+    const navigationLinks = screen.getAllByRole('listitem');
+
     expect(navigationLinks[0].textContent).toBe('link');
 });
 
 test('links to a newtab if newTab is set to true in linkObject', () => {
+    render(<Homepage />);
+    const navigationLinksAnchor = screen.getAllByRole('link');
+
     expect(navigationLinksAnchor[2].target).toBe('_blank');
 });
 
 test('does not link to a newtab if newTab is set to false or was not specified', () => {
+    render(<Homepage />);
+    const navigationLinksAnchor = screen.getAllByRole('link');
     expect(navigationLinksAnchor[0].target).toBe('_self');
     expect(navigationLinksAnchor[1].target).toBe('_self');
 });
