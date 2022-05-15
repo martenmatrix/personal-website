60cb48d001bc9a0de404bf9e8c9610b2b3341049
martenmatrix <76761713+martenmatrix@users.noreply.github.com>
2022-05-15 14:53:30 +0200

    Add test for image component

--- src/__tests__/Image.test.js
+++ src/__tests__/Image.test.js
@@ -1,1 +1,17 @@
-test.todo('displays loading animation before image loads')
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Image from '../components/Image';
+
+jest.mock('react-lazy-load-image-component', () => ({
+    __esModule: true,
+    LazyLoadImage: (props) => <img {...props} />,
+}));
+
+test('removes loading class when image has loaded', () => {
+    render(<Image src="https://example.org/image.png" alt="image does not exist" />);
+    const image = screen.getByAltText('image does not exist');
+
+    expect(image.parentElement).toHaveClass('loading');
+    fireEvent.load(image);
+    expect(image.parentElement).not.toHaveClass('loading');
+});
