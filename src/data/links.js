0c0bbe85e3b85445b74d311dd905a99d0a45f3ba
Marten <derapfelesser@aol.de>
2024-04-17 14:48:19 +0200

    update mail

--- src/data/links.js
+++ src/data/links.js
@@ -1,5 +1,5 @@
 const links = [
-    { title: 'contact', href: 'mailto:privacy.shadier_0b@icloud.com?body=⚠️ I hide my real mail address to protect me from spammers. Your mail will get forwarded to my real account and I will answer with my mail address unhidden.' },
+    { title: 'contact', href: 'mailto:me@marten.codes?body=Hey Marten,' },
     { title: 'projects', href: 'projects' },
     { title: 'github', href: 'https://github.com/martenmatrix', newTab: true },
     { title: 'stack overflow', href: 'https://stackoverflow.com/users/14231863/marten', newTab: true }
